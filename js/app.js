ec142a293195dfbb8a1248942cfee5cb58e52431
jamil4321 <jamil.ahmed.ja28@mail.com>
2020-01-21 21:00:55 +0500

    add allfeature

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 // initiate Variable
+const addPatientModal = document.getElementById('addPatient')
 const paitentID = document.getElementById("paitntID");
 const name = document.getElementById("name");
 const age = document.getElementById("age");
@@ -9,9 +10,19 @@ const medication = document.getElementById("medication");
 const btnAddPaitent = document.getElementById("btnAddPaitent");
 const showPaitent = document.getElementById("showPatient");
 const notification = document.getElementById("notification");
+const searchByName = document.getElementById('searchByName');
+const radioButton = document.getElementsByName("gender");
+const modalName = document.getElementById('modalName');
+let gender ;
 let paitentData = [];
 let pid;
+
+// Add Button Function
 function addButton() {
+  modalName.innerHTML = "Add Paitent"
+  name.disabled = false;
+  age.disabled = false;
+  number.disabled = false;
   if (paitentData.length == 0) {
     pid = "p-1";
   } else {
@@ -21,15 +32,14 @@ function addButton() {
     pid[1] = id;
     pid = pid.join("-");
   }
-
+  for (i = 0; i < radioButton.length; i++) {
+    if (radioButton[i].checked) radioButton[i].checked = false;
+  }
   paitentID.value = pid;
   paitentID.disable = true;
   name.value = "";
   age.value = "";
   number.value = "";
-  disease.value = "";
-  date.value = "";
-  medication.value = "";
 }
 // View DATA in HTML
 db.ref("patients/").on("value", snapshot => {
@@ -37,14 +47,29 @@ db.ref("patients/").on("value", snapshot => {
   if (!snapshot.val()) return;
   paitentData = Object.values(snapshot.val());
   for (let i = 0; i < paitentData.length; i++) {
+    let key = paitentData[i].id.split("-");
+    // nameArr.push(paitentData[i].name.toLowerCase())
     showPaitent.innerHTML += `
         <div class="col-3 mt-3" >
-        <div class="card text-center">
-            <div class="card-body card-shaddow">
-                <h5 class="card-title">${paitentData[i].name}</h5>
-                <p class="card-text">${paitentData[i].number}</p>
-                <p class="card-text">${paitentData[i].id}</p>
-                <button class="btn btn-info" type="button" data-toggle="modal" data-target="#addPatient" onclick="getPaitent(${paitentData[i].id})">View History</button>
+        <div class="card text-center card-shaddow">
+            <div class="card-body ">
+                <h5 class="card-title">Name : <strong>${paitentData[i].name.toUpperCase()}</strong></h5>
+                <p class="card-text">Number : ${paitentData[i].number.toUpperCase()}</p>
+                <p class="card-text">Age : ${paitentData[i].age.toUpperCase()}</p>
+                <p class="card-text">Gender : ${paitentData[i].gender.toUpperCase()}</p>
+                <p class="card-text">Paitent Id : ${paitentData[i].id.toUpperCase()}</p>
+                <button class="btn btn-info" type="button" data-toggle="modal" 
+                data-target="#addPatient" onclick="getPaitent(${key[1]})">
+                <i class="fas fa-search"></i>
+                </button>
+                <button class="btn btn-warning" type="button" data-toggle="modal" 
+                data-target="#addPatient" onclick="getPaitentForUpdate(${key[1]})">
+                <i class="fas fa-user-edit"></i>
+                </button>
+                <button class="btn btn-danger" type="button" data-toggle="modal" 
+                data-target="#delet-paitent" onclick="deletePaitent(${key[1]})">
+                <i class="far fa-trash-alt"></i>
+                </button>
             </div>
         </div>`;
   }
@@ -52,22 +77,16 @@ db.ref("patients/").on("value", snapshot => {
 
 // Add Paitent Data
 function addPaitent() {
-  if (
-    name.value != "" &&
-    age.value != "" &&
-    number.value != "" &&
-    disease.value != "" &&
-    date.value != "" &&
-    medication.value != ""
-  ) {
+  for (i = 0; i < radioButton.length; i++) {
+    if (radioButton[i].checked) gender = radioButton[i].value;
+  }
+  if (name.value != "" && age.value != "" && number.value != "" && gender) {
     db.ref("patients/" + pid).set({
       id: pid,
-      name: name.value,
-      age: age.value,
-      number: number.value,
-      disease: disease.value,
-      date: date.value,
-      medication: medication.value
+      name: name.value.toLowerCase(),
+      age: age.value.toLowerCase(),
+      number: number.value.toLowerCase(),
+      gender:gender.toLowerCase()
     });
     notification.innerHTML = `
     <div class="alert alert-success alert-dismissible fade show" role="alert">
@@ -76,7 +95,7 @@ function addPaitent() {
           <span aria-hidden="true">&times;</span>
           </button>
     </div>
-    `
+    `;
   } else {
     notification.innerHTML = `
     <div class="alert alert-danger alert-dismissible fade show" role="alert">
@@ -85,15 +104,80 @@ function addPaitent() {
           <span aria-hidden="true">&times;</span>
           </button>
     </div>
-    `
+    `;
   }
 }
 // view Paitent
-
-function getPaitent(getId){
-  console.log(getId)
-  // db.ref("patients/"+getId).once('value',data =>{
-  //   dbData = data.val();
-   
-  // })
+function getPaitent(getId) {
+  modalName.innerHTML = "View Paitent"
+  name.disabled = true;
+  age.disabled = true;
+  number.disabled = true;
+  db.ref("patients/p-" + getId).once("value", data => {
+    dbData = data.val();
+    name.value = dbData.name;
+    paitentID.value = dbData.id;
+    age.value = dbData.age;
+    number.value = dbData.number;
+    if(dbData.gender == 'male'){
+      radioButton[1].checked = true
+    }else{
+      radioButton[0].checked = true
+    }
+  });
+  btnAddPaitent.onclick = ""
 }
+
+// getData for Upadte
+function getPaitentForUpdate(getId) {
+  modalName.innerHTML = "Update Paitent"
+  name.disabled = false;
+  age.disabled = false;
+  number.disabled = false;
+  db.ref("patients/p-" + getId).once("value", data => {
+    dbData = data.val();
+    name.value = dbData.name;
+    paitentID.value = dbData.id;
+
+    age.value = dbData.age;
+    number.value = dbData.number;
+    if(dbData.gender == 'male'){
+      radioButton[1].checked = true
+    }else{
+      radioButton[0].checked = true
+    }
+  });
+  btnAddPaitent.onclick = ()=>{updatePaitnet(paitentID,name,age,number)}
+}
+// UpdatePaintent
+function updatePaitnet(id,pname,page,pnumber){
+  for (i = 0; i < radioButton.length; i++) {
+    if (radioButton[i].checked) gender = radioButton[i].value;
+  }
+  db.ref('patients/'+id.value).set({
+    id:id.value,
+    name: pname.value.toLowerCase(),
+    age: page.value.toLowerCase(),
+    number: pnumber.value.toLowerCase(),
+    gender:gender.toLowerCase(),
+  })
+}
+
+// delete Paitent
+function deletePaitent(getId){
+  db.ref("patients/p-" + getId).remove()
+}
+
+
+// Search
+// let nameArr = [];
+searchByName.addEventListener('keyup',(e)=>{
+  let key = e.target.value.toLowerCase()
+  paitentData.forEach((name)=>{
+    if(name.indexOf(key) != -1){
+      console.log(name)
+    }else{
+      console.log('no item found')
+    }
+  })
+})
